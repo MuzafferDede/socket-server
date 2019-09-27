fcfcc4b7915c5adad0c1e71fc865c04d57f0f6e1
muzaffer <muzaffer@laxan.com.tw>
2019-09-27 10:49:11 +0800

    fixed localhost url

--- src/Server.php
+++ src/Server.php
@@ -55,7 +55,7 @@ class Server
                 }
 
                 $client = new Client([
-                    'base_uri' => 'http://127.0.0.1',
+                    'base_uri' => 'http://localhost',
                     //'http_errors' => false,
                     //'debug' => true
                 ]);
